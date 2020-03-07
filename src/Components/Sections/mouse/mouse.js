a900940e82fb5d94a42bd6d76fdac9979643ee10
Mohamud Dayib <Mohmuddayib@gmail.com>
2020-03-07 20:19:08 +0300

    mouse style change

--- src/Components/Sections/mouse/mouse.js
+++ src/Components/Sections/mouse/mouse.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./mouse.css";
-import Icon from "@mdi/react";
-import { mdiCheckboxBlankCircle } from "@mdi/js";
+// import Icon from "@mdi/react";
+// import { mdiCheckboxBlankCircle } from "@mdi/js";
 import styled, { keyframes } from "styled-components";
 import { fadeOutDown } from "react-animations";
 
@@ -14,7 +14,7 @@ const Mouse = () => {
   return (
     <div className="Mouse">
       <BouncyDiv>
-        <Icon className="wheel" path={mdiCheckboxBlankCircle} size={0.5} color="#6F65EC" />
+        <div className="circle"></div>
       </BouncyDiv>
     </div>
   );
