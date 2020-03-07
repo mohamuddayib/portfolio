006aa0d7c77cfd8118bd411822c5b47817025c53
PC-Admin <Mohamud Ali Farah>
2020-03-07 12:57:11 +0300

    .

--- src/Components/Sections/fullscreen/Fullscreen.js
+++ src/Components/Sections/fullscreen/Fullscreen.js
@@ -11,8 +11,7 @@ const Fullscreen = () => {
           <h2>Web developer</h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem at aliquid amet aperiam
-            facilis explicabo aspernatur assumenda dicta nihil perferendis quaerat, eum quidem rem
-            consectetur cupiditate inventore sapiente culpa adipisci!
+            facilis explicabo aspernatur assumenda dicta nihil perferendis.
           </p>
           <button>Request a quote</button>
         </div>
