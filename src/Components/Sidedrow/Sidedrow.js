18e337a52d460c810d4ea5a05aabe08428a104f0
Mohamud Dayib <Mohmuddayib@gmail.com>
2020-03-18 19:56:27 +0300

    update responsive about section

--- src/Components/Sidedrow/Sidedrow.js
+++ src/Components/Sidedrow/Sidedrow.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./Sidedrow.css";
 import SlideLogo from "./Logo-scroll.png";
-import Icon from '@mdi/react';
-import { mdiClose } from '@mdi/js';
+import Icon from "@mdi/react";
+import { mdiClose } from "@mdi/js";
 import { Link } from "react-scroll";
-
 
 const Sidedrow = props => {
   let siderawClasses = "side-Drawer";
@@ -14,31 +13,51 @@ const Sidedrow = props => {
   return (
     <div className={siderawClasses}>
       <div className="slide-header">
-        <a href="http://mohamud.dev" className="logo-container"><img src={SlideLogo} alt="logo"/></a>
-        <Icon className="close-icon" path={mdiClose} title="Close" size={1.1} onClick={props.close} />
+        <a href="http://mohamud.dev" className="logo-container">
+          <img src={SlideLogo} alt="logo" />
+        </a>
+        <Icon
+          className="close-icon"
+          path={mdiClose}
+          title="Close"
+          size={1.1}
+          onClick={props.close}
+        />
       </div>
       <nav>
         <ul>
           <li>
-          <Link
-    activeClass="active"
-    to="section1"
-    spy={true}
-    smooth={true}
-    offset={-70}
-    duration= {500}
->Services</Link>
+            <Link
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Services
+            </Link>
           </li>
-          <li>About</li>
+          <li><Link
+              activeClass="active"
+              to="About"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              About
+            </Link></li>
           <li>Pricing</li>
           <li>Projects</li>
         </ul>
       </nav>
       <div className="ctaContact">
-      <button>Contact</button>
+        <button>Contact</button>
       </div>
     </div>
   );
 };
 
 export default Sidedrow;
+
