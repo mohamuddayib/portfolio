121c57908f10ee65490bb07f5f732ea6e7fb65cc
Mohamud Dayib <Mohmuddayib@gmail.com>
2020-03-24 20:10:31 +0300

    update resposive pricing section

--- src/Components/Sections/Payments/standard.js
+++ src/Components/Sections/Payments/standard.js
@@ -14,7 +14,7 @@ class Standard extends Component {
         </Link>
         <section>
           <PayPalButton
-            amount="1"
+            amount="199"
             // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
             onSuccess={(details, data) => {
               alert("Transaction completed by " + details.payer.name.given_name);
