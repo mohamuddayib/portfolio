c1799559e945f6ca4eedd92b4c81292d7f84e622
Mohamud Dayib <Mohmuddayib@gmail.com>
2020-03-29 07:26:29 +0300

    update contact

--- src/Components/Sections/Contact/Contact.js
+++ src/Components/Sections/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="title-container">
-        <h2>Get in touch</h2>
+        <h2>Contact me</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officia veniam
           asperiores.
