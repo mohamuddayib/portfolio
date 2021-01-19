a4a498ce146589f73d1da991c3d9184b8e222122
unknown <mohamuddev>
2021-01-19 21:50:13 +0300

    update project

--- src/Components/Sections/Projects/Project.js
+++ src/Components/Sections/Projects/Project.js
@@ -1,121 +1,78 @@
 import React, { useState, useEffect } from "react";
-import "./Project.css";
 import Icon from "@mdi/react";
 import { mdiWeb } from "@mdi/js";
 import { mdiGithubCircle } from "@mdi/js";
 
+import "./Project.css";
+
 const portfolio = [
   {
-    name: "Reference App",
-    class: "referenceApp",
-    button: (
-      <div className="buttons">
-        <h4>Reference App</h4>
-        <section>
-          <a
-            href="https://github.com/mohmuddev/reference"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon path={mdiGithubCircle} size={0.9} />
-            Github
-          </a>
-          <a
-            href="https://docs.mohamud.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon path={mdiWeb} color="#00FF00" size={0.9} />
-            Live
-          </a>
-        </section>
-      </div>
+    name: "My best client",
+    class: "jabiclc",
+    overlay:(
+      <span className="overlay_animation">
+        <span>Jabiclc</span>
+        <span><a href="https://jabiclc.com/"><Icon path={mdiWeb} color="#7D5AF8" size={1} /></a></span>
+      </span>
     ),
-    category: ["all", "webapp"],
+    category: ["all", "frontend", "ux-ui"],
   },
   {
-    name: "Web Agency",
+    name: "My favorite case",
     class: "webagency",
-    button: (
-      <div className="buttons">
-        <h4>Digital Agency</h4>
-        <section>
-          <a
-            href="https://github.com/mohmuddev/WebAgency"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            <Icon path={mdiGithubCircle} size={0.9} />
-            Github
-          </a>
-          <a
-            href="https://digitalagency.mohamud.dev/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {" "}
-            <Icon path={mdiWeb} color="#00FF00" size={0.9} />
-            Live
-          </a>
-        </section>
-      </div>
+    overlay:(
+      <span className="overlay_animation">
+        <span>Web Agency</span>
+        <span><a href="https://github.com/mohmuddev/WebAgency"><Icon path={mdiGithubCircle} color="#7D5AF8" size={1} /></a> <a href="https://digitalagency.mohamud.dev/"><Icon path={mdiWeb} color="#7D5AF8" size={1} /></a></span>
+      </span>
     ),
-    category: ["all", "landingPage"],
+    category: ["all", "mobile", "ux-ui"]
   },
   {
-    name: "Happy New Year",
-    class: "HappyNewYear ",
-    button: (
-      <div className="buttons">
-        <h4>Secured docs app</h4>
-        <section>
-          <a href="https://github.com/mohmuddev/ishraq-docs">
-            <Icon path={mdiGithubCircle} size={1} />
-            Github
-          </a>
-          <a
-            href="https://ishraq-docs.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon path={mdiWeb} color="#00FF00" size={1} />
-            Live
-          </a>
-        </section>
-      </div>
+    name: "A old job",
+    class: "israqdocs",
+    overlay:(
+      <span className="overlay_animation">
+        <span>Docs App</span>
+        <span><a href="https://github.com/mohmuddev/ishraq-docs"><Icon path={mdiGithubCircle} color="#7D5AF8" size={1} /></a> <a href="https://ishraq-docs.netlify.app"><Icon path={mdiWeb} color="#7D5AF8" size={1} /></a></span>
+      </span>
     ),
-    category: ["all", "webapp"],
+    category: ["all", "frontend"]
   },
-
   {
-    name: "Saas Newsletter ",
-    class: "saas",
-    button: (
-      <div className="buttons">
-        <h4>Covid-19 tracker</h4>
-        <section>
-          <a
-            href="https://github.com/mohmuddev/coronavirus"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon path={mdiGithubCircle} size={0.9} />
-            Github
-          </a>
-          <a
-            href="https://coronavirus.mohamud.dev/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon path={mdiWeb} color="#00FF00" size={0.9} />
-            Live
-          </a>
-        </section>
-      </div>
+    name: "It is a really cool website",
+    class: "molfr",
+    overlay:(
+      <span className="overlay_animation">
+        <span>MOLFR-JSS</span>
+        <span> <a href="https://molfr.so/"><Icon path={mdiWeb} color="#7D5AF8" size={1} /></a></span>
+      </span>
     ),
-    category: ["all", "webapp"],
+    category: ["all", "frontend", "ux-ui"]
   },
+  {
+    name: "Something more",
+    class: "weedsan",
+    overlay:(
+      <span className="overlay_animation">
+        <span>Weedhsan News</span>
+        <span><a href="https://www.weedhsan.news/"><Icon path={mdiWeb} color="#7D5AF8" size={1} /></a></span>
+      </span>
+    ),
+    category: ["all", "others"]
+  },
+  
+  {
+    name: "loop div",
+    class: "BMI_calculator",
+    overlay:(
+      <span className="overlay_animation">
+        <span>BMI Calculator</span>
+        <span><a href="https://mohmuddev.github.io/bmi/"><Icon path={mdiGithubCircle} color="#7D5AF8" size={1} /></a> <a href="https://github.com/mohmuddev/bmi"><Icon path={mdiWeb} color="#7D5AF8" size={1} /></a></span>
+      </span>
+    ),
+    category: ["all", "others"]
+  }
 ];
 
 function Project() {
@@ -129,58 +86,60 @@ function Project() {
   useEffect(() => {
     setProjects([]);
 
-    const filtered = portfolio.map((p) => ({
+    const filtered = portfolio.map(p => ({
       ...p,
-      filtered: p.category.includes(filter),
+      filtered: p.category.includes(filter)
     }));
     setProjects(filtered);
   }, [filter]);
   return (
-    <div className="Propect" id="Project">
-      <div className="title-container">
+    <div className="project-container">
+    <div className="title-container">
         <h2>My Work</h2>
         <p>
           I have showcased below some of my recent projects and I'm currently
           working on new projects.
         </p>
       </div>
-
+    <div className="portfolio_wrapper">
       <div className="portfolio__labels">
-        <span active={filter === "all"} onClick={() => setFilter("all")}>
+        
+        <span href="/#" active={filter === "all"} onClick={() => setFilter("all")}>
           All
         </span>
-        <span active={filter === "webapp"} onClick={() => setFilter("webapp")}>
-          Web App
+        <span
+          active={filter === "frontend"}
+          onClick={() => setFilter("frontend")}
+        >
+          Frontend
         </span>
         <span
-          active={filter === "landingPage"}
-          onClick={() => setFilter("landingPage")}
+          active={filter === "mobile"}
+          onClick={() => setFilter("mobile")}
         >
-          Landing Page
+          Mobile
         </span>
         <span
-          active={filter === "html-email"}
-          onClick={() => setFilter("html-email")}
+          active={filter === "ux-ui"}
+          onClick={() => setFilter("ux-ui")}
         >
-          HTML-Email
+          UX/UI
         </span>
         <span
-          active={filter === "webdesign"}
-          onClick={() => setFilter("webdesign")}
+         
+          active={filter === "others"}
+          onClick={() => setFilter("others")}
         >
-          Web design
-        </span>
+          Others
+          </span>
       </div>
+      </div>
+      <div className="filterable_div">
       <div className="portfolio__container">
-        {projects.map((item) =>
-          item.filtered === true ? (
-            <div key={item.name} className={item.class}>
-              {item.button}
-            </div>
-          ) : (
-            ""
-          )
+        {projects.map(item =>
+        item.filtered === true ? <span key={item.name} className={item.class}>{item.overlay}</span> : ""
         )}
+      </div>
       </div>
     </div>
   );
